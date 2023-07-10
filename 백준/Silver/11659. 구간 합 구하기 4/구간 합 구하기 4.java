730484cec2199d21649265a6fc1656e5b6a59ea0
이찬우 <105884869+hallym-U-coding-king@users.noreply.github.com>
2023-07-10 19:59:12 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 1504 ms, Memory: 59148 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.java
@@ -1,25 +1,27 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		int numcnt = sc.nextInt();
-		int loopcnt = sc.nextInt();
-		sc.nextLine();
-		int Asum[]=new int[numcnt+1];
-		Asum[0]=0;
-		for(int i=1; i<numcnt+1; i++) {
-			Asum[i]=Asum[i-1]+sc.nextInt();
+		BufferedReader buffRead = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(buffRead.readLine());
+		int numcnt = Integer.parseInt(st.nextToken());
+		int loopcnt = Integer.parseInt(st.nextToken());
+		int Asum[] = new int[numcnt + 1];
+		st = new StringTokenizer(buffRead.readLine());
+		for (int i = 1; i < numcnt + 1; i++) {
+			Asum[i] = Asum[i - 1] + Integer.parseInt(st.nextToken());
 		}
-		
-		for(int i=0; i<loopcnt; i++) {
-			int start=sc.nextInt();
-			int end=sc.nextInt();
-			System.out.println(Asum[end]-Asum[start-1]);
+		for (int i = 0; i < loopcnt; i++) {
+			st = new StringTokenizer(buffRead.readLine());
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			System.out.println(Asum[end] - Asum[start - 1]);
 		}
-		
 	}
 
 }
